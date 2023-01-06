9abde97291a884933f022a3773f4b43505020419
Simon <karinn.71@gmail.com>
2023-01-06 11:40:15 +0200

    angular unit tests

--- autocomplete/projects/autocomplete/src/lib/autocomplete.component.spec.ts
+++ autocomplete/projects/autocomplete/src/lib/autocomplete.component.spec.ts
@@ -1,23 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { AutocompleteComponent } from './autocomplete.component';
 
 describe('AutocompleteComponent', () => {
-  let component: AutocompleteComponent;
-  let fixture: ComponentFixture<AutocompleteComponent>;
+	let component: AutocompleteComponent;
+	let fixture: ComponentFixture<AutocompleteComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AutocompleteComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [AutocompleteComponent]
+		})
+		.compileComponents();
 
-    fixture = TestBed.createComponent(AutocompleteComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(AutocompleteComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should be auto-filled datalistId', () => {
+		expect(component.datalistId).toBeTruthy();
+		console.log(component.datalistId)
+	});
+
+	
+	it('should have defaults properly set', () => {		
+		expect(component.label).toBeUndefined();
+		expect(component.placeholder).toEqual('');
+		expect(component.options).toEqual([]);
+		expect(component.src).toBeFalsy();
+		expect(component.disabled).toBeFalse();
+		expect(component.minLength).toEqual(2);
+		expect(component.maxResults).toEqual(10);
+		expect(component.value).toBeFalsy();
+	});	
 });
